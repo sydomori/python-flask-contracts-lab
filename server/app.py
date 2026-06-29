47bcbedfa579461242e2385399e953933046f98a
sydomori <sydneyomori@gmail.com>
2026-06-29 22:13:16 +0300

    fx: change Contract

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, current_app, g, make_response
 
-contracts = [{"id": 1, "contract_information": "This contract is for John and building a shed"},{"id": 2, "contract_information": "This contract is for a deck for a buisiness"},{"id": 3, "contract_information": "This contract is to confirm ownership of this car"}]
+contracts = [{"id": 1, "contract_information": "This Contract is for John and building a shed"},{"id": 2, "contract_information": "This contract is for a deck for a buisiness"},{"id": 3, "contract_information": "This contract is to confirm ownership of this car"}]
 customers = ["bob","bill","john","sarah"]
 app = Flask(__name__)
 
